e9a73a70d61c5cb91a01b002e4cfe67d7b0419d9
Apolloniann <zeeshangill537@gmail.com>
2022-12-03 17:51:33 +0500

    complete CRUD API's of SuperHeros

---
+++ SuperHeros/Migrations/20221130052719_CreatIntail.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SuperHeros.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatIntail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SuperHeroes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ChacterName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FristName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SuperHeroes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SuperHeroes");
+        }
+    }
+}
